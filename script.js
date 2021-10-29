7065b864e8b780330986ff4f2d0092362c921436
Variosity <DepravedCallous84@outlook.com>
2021-10-29 17:51:37 -0400

    The brunt of our website
    A Start.

---
+++ script.js
@@ -0,0 +1,10 @@
+(function(){
+  "use strict";
+  /* Start of your code */
+  function greetMe(yourName) {
+    alert('Hello ' + yourName);
+  }
+
+  greetMe('Adrian');
+  /* End of your code */
+})();
